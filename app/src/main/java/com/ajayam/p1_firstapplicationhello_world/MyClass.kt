106e864600bd0b875d37165092a6edcbb7c2021b
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-05-29 17:20:16 +0530

    kotlin_basics: conditional statement

--- app/src/main/java/com/ajayam/p1_firstapplicationhello_world/MyClass.kt
+++ app/src/main/java/com/ajayam/p1_firstapplicationhello_world/MyClass.kt
@@ -1,17 +1,69 @@
 package com.ajayam.p1_firstapplicationhello_world
 
 class MyClass {
-    companion object{
+    companion object {
         @JvmStatic
-        fun main(args: Array<String>){
+        fun main(args: Array<String>) {
 
-            println("The sum of A and B is ${add(5, 6, 11)}")
-         }
-        fun add(a:Int, b:Int) : Int {
-            return a+b
-        }
-        fun add(a:Int, b: Int, c:Int) :  Int {
-            return a+b+c
+            var num = 11
+// way of writing 1
+//            if (num > 100) {
+//                println("No is grater")
+//            } else {
+//                println("No is smaller")
+//            }
+
+// way of writing 2 ({} not important on if-else)
+//            if (num > 100)
+//                println("No is grater")
+//            else
+//                println("No is smaller")
+
+// way of writing 3 (single if-else can write on single line)
+            var msg = ""
+            msg =  if (num > 100) "No is grater" else "No is smaller"
+            println(msg)
+
+
+// nested conditional
+            if(num>100){
+
+            } else if (num<70) {
+
+            } else if (num< 50) {
+
+            } else {
+
+            }
+
+// WHEN in place of switch condition
+            when(num){
+                1->{
+//                -> sign of equal to(=) eg. 1=num
+                }
+                100-> {
+
+                }
+            }
+
+// conditional operators used
+            when{
+                num>100 -> {
+
+                }
+                num<100->{
+
+                }
+                num==100->{
+
+                }
+                else -> {
+
+                }
+            }
+
+
+
         }
     }
 }
