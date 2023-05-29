3061d8a23b87a5acdc13c55fdafb563b67254430
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-05-29 19:55:18 +0530

    kotlin_basics: while & do-while loop conditions

--- app/src/main/java/com/ajayam/p1_firstapplicationhello_world/MyClass.kt
+++ app/src/main/java/com/ajayam/p1_firstapplicationhello_world/MyClass.kt
@@ -6,41 +6,19 @@ class MyClass {
         fun main(args: Array<String>) {
             println("Hello world")
 
-            var num = 0
+            var num = 10;
 
-// run 0 to 10
-//            for (i in 0..10){
-//                println("Number is : ${num++}")
+//   while loop
+//            while (num<20){
+//                println("Num is: $num")
+//                num++;
 //            }
 
-// run 0 to 9
-//            for (i in 0 until 10){
-//                println("Number is : ${num++}")
-//            }
+//   do-while loop
+            do {
+                println("Num is : $num")
+            } while (num>10)
 
-// run reverse
-//            for (i in 10 downTo 0){
-//                println("Number is : ${num++}")
-//            }
-
-//  run between 2 step
-//            for (i in 0..10 step 2){
-//                println("Number is : ${num++}")
-//            }
-
-// run array list
-            val arrNo = ArrayList<Int>()
-            arrNo.add(1)
-            arrNo.add(2)
-            arrNo.add(3)
-            arrNo.add(4)
-            arrNo.add(5)
-            arrNo.add(6)
-            arrNo.add(7)
-
-            for (i in arrNo){
-                println("Number is : $i")
-            }
 
         }
     }
