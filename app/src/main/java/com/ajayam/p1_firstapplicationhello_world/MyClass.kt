f6d2def2482f8991d9be0a432ba37f4a42d1ec04
sangwa2529 <99901068+sangwa2529@users.noreply.github.com>
2023-05-29 17:03:21 +0530

    kotlin_basics: methods in kotlin

--- app/src/main/java/com/ajayam/p1_firstapplicationhello_world/MyClass.kt
+++ app/src/main/java/com/ajayam/p1_firstapplicationhello_world/MyClass.kt
@@ -4,25 +4,14 @@ class MyClass {
     companion object{
         @JvmStatic
         fun main(args: Array<String>){
-// Var = can change the data at runtime
-// val = can't change the data at runtime(it is fixed)           va
-            var a = 11
-            var b = "Hello"
-//            val b = "Hello"
 
-            b = "World"
-
-            a = 7
-
-            println("$a, $b")
-
-//            println("The sum of A and B is ${add(5, 6, 11)}")
+            println("The sum of A and B is ${add(5, 6, 11)}")
          }
-//        fun add(a:Int, b:Int) : Int {
-//            return a+b
-//        }
-//        fun add(a:Int, b: Int, c:Int) :  Int {
-//            return a+b+c
-//        }
+        fun add(a:Int, b:Int) : Int {
+            return a+b
+        }
+        fun add(a:Int, b: Int, c:Int) :  Int {
+            return a+b+c
+        }
     }
 }
